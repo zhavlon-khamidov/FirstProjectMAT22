7c5862338a91179b7bfa77be0da3796d5f2a2b53
Mr.Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2023-10-18 15:35:04 +0600

    create post feature added

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,5 +4,6 @@ from blogapp import views
 
 urlpatterns = [
     path('',views.all_posts),
-    path('post/<str:id>', views.post_by_id, name="post_by_id")
+    path('post/<str:id>', views.post_by_id, name="post_by_id"),
+    path('create/post', views.create_post, name="create_post")
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,11 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 from blogapp.models import Post
 
 # Create your views here.
 
-def all_posts(request):
+def all_posts(request:HttpRequest):
     context = {
         'posts':Post.objects.all()
     }
@@ -20,3 +21,16 @@ def post_by_id(request, id):
         'post' : Post.objects.get(id=id)
     }
     return render(request,'blogapp/post.html', context=context)
+
+
+@csrf_exempt
+def create_post(request:HttpRequest):
+    if(request.method == 'POST'):
+        print("POST request handled")
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        print('title = ', title)
+        print('content = ', content)
+        new_post = Post(title = title, content=content)
+        new_post.save()
+    return render(request, "blogapp/post-form.html")
